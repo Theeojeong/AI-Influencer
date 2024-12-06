ec4f96734082d9629275ff000f89f943b094a9bf
KSE <szn3700@gmail.com>
2024-12-06 09:07:01 +0900

    Merge branch "frontend"

--- backend/app/auth/token.py
+++ backend/app/auth/token.py
@@ -1,12 +1,35 @@
 # app/auth/token.py
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
 from app.common.config import get_parameter
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException
 
 SECRET_KEY = get_parameter("/MYAPP/GOOGLE/AUTH/SECRETKEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 15
 REFRESH_TOKEN_EXPIRE_DAYS = 7
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/google")  # 로그인 엔드포인트
+
+# 토큰을 검증하고 유저 정보를 추출
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        exp = payload.get("exp")
+        if exp and datetime.utcnow().timestamp() > exp:
+            raise HTTPException(status_code=401, detail="Token expired")
+        return payload
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+# 인증된 사용자 확인
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    payload = verify_access_token(token)
+    user_email = payload.get("sub")
+    if not user_email:
+        raise HTTPException(status_code=401, detail="Invalid user")
+    return user_email
 
 def create_access_token(data: dict):
     """
